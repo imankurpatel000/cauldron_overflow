ff75d35393c70030bcdbc04cea2dbff74c7f2053
Ankur <ankurp@meditab.com>
2020-05-24 16:14:28 +0530

    added twig

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,10 +3,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -24,9 +25,15 @@ class QuestionController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show the question "%s"!',
-            ucwords(str_replace('-', ' ', $slug))
-        ));
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers
+        ]);
     }
 }
